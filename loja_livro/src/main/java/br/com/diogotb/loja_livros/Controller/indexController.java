485b915995d2949b34608b9703de75f78123b170
EllysonRPT <reptile1284@gmail.com>
2024-05-16 20:10:40 -0300

    16/05

--- loja_livro/src/main/java/br/com/diogotb/loja_livros/Controller/indexController.java
+++ loja_livro/src/main/java/br/com/diogotb/loja_livros/Controller/indexController.java
@@ -22,5 +22,10 @@ public class indexController {
         ModelAndView mv = new ModelAndView("index");
              return mv ;
     }
+    @GetMapping("/livros-add")
+    public ModelAndView getLivrosAdd() {
+        ModelAndView mv = new ModelAndView("livros-add");
+             return mv ;
+    }
     
 }
